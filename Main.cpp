bc1ecd93630868f50e585f4d161ad95fed6a4038
Kangwei Liao <miaochips@Kangweis-MacBook-Pro.local>
2020-12-05 18:45:26 -0500

    Main update

--- Main.cpp
+++ Main.cpp
@@ -10,43 +10,11 @@
 #include <fstream>
 
 // project headers:
-// #include "Table.h"
-#include "MyException.h"
-#include "CardFactory.h"
-#include "Hand.h"
+#include "Table.h"
 
 using namespace std;
 
 int main() {
-    CardFactory factory;
-    Deck deck = factory.getDeck();
-    cout << deck << endl;
-
-    // write file:
-    // filebuf fb;
-    // fb.open ("save.txt",ios::out);
-    // ostream os(&fb);
-    // write ... in os
-    // fb.close();
-
-    // read file:
-    // filebuf fb;
-    // if (fb.open ("save.txt",ios::in)) {
-    //     istream is(&fb);
-    //     while (is) cout << char(is.get());
-    //     fb.close();
-    // }
-
-    // Chain<Black> black;
-    // try {
-    //     for(auto& card: deck) {
-    //         string type = typeid(*card).name();
-    //         // cout << type << endl;
-    //         if(type == typeid(Black).name()) black += card;
-    //     }
-    // } catch(const IllegalTypeException& e) {
-    //     cout << "Not black" << endl;
-    // }
-    // cout << black.getSize() << endl;
-
+    Table* table = new Table("Clancy", "Skye");
+    cout << *table;
 };
